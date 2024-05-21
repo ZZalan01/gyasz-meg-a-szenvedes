0d06f222504ee911fd9af812371acbedadd5780f
ZZalan01 <zzalan14@outlook.hu>
2024-05-21 13:54:38 +0200

    reszletek megbuheralasa

--- script.js
+++ script.js
@@ -4,16 +4,45 @@ fetch("https://pizza.kando-dev.eu/Pizza").then(function(adatok){
         console.log(adatok)
         let contentHTML = document.getElementById("content")
         contentHTML.innerHTML = "";
+        
                 for (let i = 0; i < adatok.length; i++) {
                     contentHTML.innerHTML += `<div class="card col-sm-3" style="width:18rem;">
                     <img src="${adatok[i].kepURL}" class="card-img-top" alt="${adatok[i].name}">
                     <div class="card-body">
                     <h5 class="card-title">${adatok[i].name}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Gluténmentes: ${adatok[i].isGlutenFree}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Gluténmentes: ${adatok[i].isGlutenFree ? "Igen" : "Nem"}</h6>
                     <h5 class="card-title">Ár: ${adatok[i].id * "18"}  </h5>
                     <button>Kosárba</button>
+                    <button onclick="Reszletek(${adatok[i].id})">Részletek</button>
                 </div>
                 </div>
                 </div>`
                 }
     })
+
+function Reszletek(id){
+    console.log(id);
+/*
+    fetch("https://pizza.kando-dev.eu/Pizza").then(function(adatok){
+        return adatok.json();
+    }).then(function (adatok){
+        console.log(adatok)
+        let contentHTML = document.getElementById("content")
+        contentHTML.innerHTML = "";
+        
+                for (let i = 0; i < adatok.length; i++) {
+                    contentHTML.innerHTML += `<div class="card col-sm-3" style="width:18rem;">
+                    <img src="${adatok[i].kepURL}" class="card-img-top" alt="${adatok[i].name}">
+                    <div class="card-body">
+                    <h5 class="card-title">${adatok[i].name}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Gluténmentes: ${adatok[i].isGlutenFree ? "Igen" : "Nem"}</h6>
+                    <h5 class="card-title">Ár: ${adatok[i].id * "18"}  </h5>
+                    <button>Kosárba</button>
+                    <button onclick="Reszletek(${adatok[i].id})">Részletek</button>
+                </div>
+                </div>
+                </div>`
+                }
+    })
+    */
+}
