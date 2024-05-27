624a264cc6a2cef260efa0918d2253328aa72ae0
ZZalan01 <zzalan14@outlook.hu>
2024-05-27 22:16:44 +0200

    orai post delete

--- script.js
+++ script.js
@@ -13,36 +13,40 @@ fetch("https://pizza.kando-dev.eu/Pizza").then(function(adatok){
                     <h6 class="card-subtitle mb-2 text-muted">Gluténmentes: ${adatok[i].isGlutenFree ? "Igen" : "Nem"}</h6>
                     <h5 class="card-title">Ár: ${adatok[i].id * "18"}  </h5>
                     <button>Kosárba</button>
-                    <button onclick="Reszletek(${adatok[i].id})">Részletek</button>
+                    <button id="reszlet">Részletek</button>
                 </div>
                 </div>
                 </div>`
                 }
     })
 
-function Reszletek(id){
-    console.log(id);
-/*
-    fetch("https://pizza.kando-dev.eu/Pizza").then(function(adatok){
+function SingleData(id){
+    fetch("https://pizza.kando-dev.eu/Pizza/"+id).then(function(adatok){
         return adatok.json();
-    }).then(function (adatok){
-        console.log(adatok)
+    }).then(function(adatok){
         let contentHTML = document.getElementById("content")
-        contentHTML.innerHTML = "";
-        
-                for (let i = 0; i < adatok.length; i++) {
-                    contentHTML.innerHTML += `<div class="card col-sm-3" style="width:18rem;">
+        contentHTML.innerHTML += `<div class="card col-sm-3" style="width:18rem;">
                     <img src="${adatok[i].kepURL}" class="card-img-top" alt="${adatok[i].name}">
                     <div class="card-body">
                     <h5 class="card-title">${adatok[i].name}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">Gluténmentes: ${adatok[i].isGlutenFree ? "Igen" : "Nem"}</h6>
                     <h5 class="card-title">Ár: ${adatok[i].id * "18"}  </h5>
                     <button>Kosárba</button>
-                    <button onclick="Reszletek(${adatok[i].id})">Részletek</button>
+                    <button id="reszlet">Részletek</button>
                 </div>
                 </div>
                 </div>`
-                }
+}
+
+    )}
+
+function Delete(id){
+    fetch("https://pizza.kando-dev.eu/Pizza/"+id, {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json"
+    }   
     })
-    */
+    .then(function(){
+        location.reload();
+    })
 }
